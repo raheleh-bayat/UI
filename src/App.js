ba3ae6dc23ea94bd4730c9c0d04500c61768f05f
raheleh <raheleh.bayat.92@gmail.com>
2022-09-16 12:51:56 +0200

    i finished the style components

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import 'react-bootstrap-buttons/dist/react-bootstrap-buttons.css';
-// import Index from './Component/index';
-// import Container from './Component/Room ';
 import Button from "./Component/Button/index"
-// import {Button,ButtonLable} from './Component/Button.style'
 
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Index/> */}
       <Button/>
-      {/* <Button backgroundColor="red"> <ButtonLable>salam</ButtonLable></Button>
-      <Button backgroundColor="green"> salam </Button>
-      <Button backgroundColor="blue"> salam </Button> */}
+     
     </div>
   );
 }
---
+++ src/Component/Footer/footer.style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Wrrap = styled.div`
+  max-width: 100%;
+  height: 50px;
+  border: 1px solid lightblue;
+  margin-top: 10px;
+  border-radius: 5px;
+  background-color: rgb(43, 92, 177);
+  /* display: none; */
+  position: relative;
+  color: white;
+  font-size: large;
+`;
+export const Inner = styled.div``;
+
+export const FooterSearch = styled.svg`
+  margin-top: 15px;
+  position: absolute;
+  left: 600px;
+`;
+export const Footerp = styled.p`
+  position: absolute;
+  left: 630px;
+  top: 10px;
+`;
